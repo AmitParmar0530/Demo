2c56a5dffc20aefcaa10aa27f6a916b8b393b8e7
amit <amit@DESKTOP-32AE8BL>
2018-12-23 15:54:57 +0530

    First Commit

---
+++ Test/src/com/demo/main/Helloworld.java
@@ -0,0 +1,10 @@
+package com.demo.main;
+/* Save this in a file called Main.java to compile and test it */
+
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
